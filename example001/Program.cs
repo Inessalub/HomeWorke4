8c32176a9924bcc31442ba5a2f6e972d312a6b40
unknown <inessalub@gmail.com>
2022-10-17 12:03:42 +0400

    example001

---
+++ example001/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+int [] array = {6,4,7,4,9,3,6,0};
+for (int i = 0; i < array.Length; i++)
+    {
+    Console.WriteLine($"{array[i]}");
+    }
+Console.WriteLine();    
